487bdc67203e41569eae9c50cbebe32861647f40
Gabriel Ribeiro Paiva <gabrielpaiva2002@gmail.com>
2023-08-17 23:27:48 -0300

    test: add option to only display a certain amount of clock cycles tests

--- Tester.v
+++ Tester.v
@@ -1,5 +1,6 @@
 module Tester();
   parameter CLK_HALF_PERIOD = 5;
+  parameter MAX_CYCLES = 1000; // Stores the number of clock cycles we want to test
   
   reg clk;
   wire [31:0] instruction;
@@ -13,7 +14,13 @@ module Tester();
     forever clk = #( CLK_HALF_PERIOD )  ~clk;
   end
 
+  integer curr_cycle = 0;
   always @(posedge clk) begin
-    $display(instruction);
+    if (curr_cycle != MAX_CYCLES) begin
+      if (instruction != 0) begin
+        $display(instruction);
+      end
+      curr_cycle++;
+    end
   end
 endmodule
