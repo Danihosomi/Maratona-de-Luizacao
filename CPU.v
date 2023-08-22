98854ee6291a61f021c4271a0e682901c1010997
LarissaEscaliante <larissastefano12@gmail.com>
2023-08-22 10:51:32 -0300

    implemented control

--- CPU.v
+++ CPU.v
@@ -2,10 +2,28 @@ module CPU(
   input clk
 );
   wire [31:0] instruction;
+  wire branch;
+  wire MemRead;
+  wire MemtoReg;
+  wire ALUOp;
+  wire MemWrite;
+  wire ALUSrc;
+  wire RegWrite;
 
   InstructionFetch instructionFetch(
     .clk(clk),
     .instruction(instruction)
+  );
+
+  Control control(
+    instruction,
+    branch,
+    MemRead,
+    MemtoReg,
+    ALUOp,
+    MemWrite,
+    ALUSrc,
+    RegWrite
   );
 
 endmodule
--- Tester.v
+++ Tester.v
@@ -1,26 +1,26 @@
 module Tester();
   parameter CLK_HALF_PERIOD = 5;
-  parameter MAX_CYCLES = 1000; // Stores the number of clock cycles we want to test
+  // parameter MAX_CYCLES = 1000; // Stores the number of clock cycles we want to test
   
   reg clk;
-  wire [31:0] instruction;
+  // wire [31:0] instruction;
 
-  InstructionFetch instructionFetch(
-    .clk(clk),
-    .instruction(instruction)
-  );
+  // InstructionFetch instructionFetch(
+  //   .clk(clk),
+  //   .instruction(instruction)
+  // );
 
-  // initial begin
-  //   forever clk = #(CLK_HALF_PERIOD) ~clk;
-  // end
-
-  integer curr_cycle = 0;
-  always @(posedge clk) begin
-    if (curr_cycle != MAX_CYCLES) begin
-      if (instruction != 0) begin
-        $display(instruction);
-      end
-      curr_cycle++;
-    end
+  initial begin
+    clk = #(CLK_HALF_PERIOD) ~clk;
   end
+
+  // integer curr_cycle = 0;
+  // always @(posedge clk) begin
+  //   if (curr_cycle != MAX_CYCLES) begin
+  //     if (instruction != 0) begin
+  //       $display(instruction);
+  //     end
+  //     curr_cycle++;
+  //   end
+  // end
 endmodule
