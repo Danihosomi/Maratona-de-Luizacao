e14f421c5c5f0212345f30e98d6cf596c24fdff0
LarissaEscaliante <larissastefano12@gmail.com>
2023-08-24 23:38:52 -0300

    fix (again) aluSrc

--- Control.v
+++ Control.v
@@ -17,7 +17,7 @@ module Control(
   assign memRead = (opcode == 'b0000011) ? 1 : 0;
   assign memToReg = (opcode == 'b0000011) ? 1 : 0;
   assign memWrite = (opcode == 'b0100011) ? 1 : 0;
-  assign aluSrc = ((opcode == 'b0000011) || (opcode == 'b0100011)) ? 1 : 0;
+  assign aluSrc = ((opcode == 'b0010011) || (opcode == 'b0000011) || (opcode == 'b0100011)) ? 1 : 0;
   assign regWrite = ((opcode == 'b0000011) || (opcode == 'b0010011) || (opcode == 'b0110011)) ? 1 : 0;
   assign aluOp = ((opcode == 'b1100011) || (opcode == 'b0010011) || (opcode == 'b0110011) || (opcode == 'b0000011) || (opcode == 'b0100011)) ? 1 : 0;
 
