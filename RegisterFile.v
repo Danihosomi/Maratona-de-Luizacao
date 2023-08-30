52b3adffc48575940a1d412fe606ed215b9a1059
Hik <luiz.alves@playkids.com>
2023-08-29 22:02:16 -0300

    fix: write on registers at the negative edge

--- RegisterFile.v
+++ RegisterFile.v
@@ -9,12 +9,12 @@ module RegisterFile(
   output [31:0] source2RegisterData
 );
   reg [31:0] registers [32];
-
+  
   assign source1RegisterData = (source1RegisterIndex == 0) ? 0 : registers[source1RegisterIndex];
   assign source2RegisterData = (source2RegisterIndex == 0) ? 0 : registers[source2RegisterIndex];
 
   // Writing at the rising edge solves the structural hazard of reading and writing at the same clock
-  always @(posedge clk) begin
+  always @(negedge clk) begin
     if (shouldWrite) begin
       registers[writeRegisterIndex] <= writeRegisterData;
     end
