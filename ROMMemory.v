7ca16647753ef282cc54d9a41ca0fd4ae3ef9d32
Gabriel Ribeiro Paiva <gabrielpaiva2002@gmail.com>
2023-09-01 16:55:49 -0300

    test: update firmware to test CPU

--- ROMMemory.v
+++ ROMMemory.v
@@ -5,13 +5,15 @@ module ROMMemory (
 
   always @* begin
     case(address[9:2]) 
-        0: data = 32'hfce08793;
-        1: data = 32'h00812703;
-        2: data = 32'h1f408067;
-        3: data = 32'h00e12423;
-        4: data = 32'h12a98663;
-        5: data = 32'h0a78f537;
-        6: data = 32'h09218c6f;
+        0: data = 32'h00a00093;
+        1: data = 32'h01400113;
+        2: data = 32'h01e00193;
+        3: data = 32'h40102023;
+        4: data = 32'h40202223;
+        5: data = 32'h40302423;
+        6: data = 32'h40002203;
+        7: data = 32'h40402283;
+        8: data = 32'h40802303;
     endcase
   end
 
--- assembly/main.asm
+++ assembly/main.asm
@@ -1,7 +1,9 @@
-addi x15, x1, -50
-lw x14, 8(x2)
-jalr x0, x1, 500
-sw x14, 8(x2)
-beq x19, x10, 300
-lui x10, 42895
-jal x24, 98450
+addi x1, x0, 10
+addi x2, x0, 20
+addi x3, x0, 30
+sw x1, 1024(x0)
+sw x2, 1028(x0)
+sw x3, 1032(x0)
+lw x4, 1024(x0)
+lw x5, 1028(x0)
+lw x6, 1032(x0)
