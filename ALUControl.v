76019b1bad0ef870ad6c6f7a2df0af234f79a063
Hik <luiz.alves@playkids.com>
2023-09-01 20:03:22 -0300

    fix: use non sequential alucontrol assignment

--- ALUControl.v
+++ ALUControl.v
@@ -5,19 +5,20 @@ module ALUControl (
     output reg [3:0] result
 );
 
-    always @* begin
+    always @(ALUOp, func3, func7) begin
+        result <= 4'b0010;
         case (ALUOp)
-            2'b00: result = 4'b0010; 
-            2'b01: result = 4'b0110;                   
-            2'b11: result = 4'b0110;               
+            2'b00: result <= 4'b0010; 
+            2'b01: result <= 4'b0110;                   
+            2'b11: result <= 4'b0110;               
             2'b10: 
                 case (func3)
-                    3'b000: result = func7[5] ? 4'b0110 : 4'b0010;
-                    3'b110: result = 4'b0001;
-                    3'b111: result = 4'b0000;
-                    default: result = 4'b0010;
+                    3'b000: result <= func7[5] ? 4'b0110 : 4'b0010;
+                    3'b110: result <= 4'b0001;
+                    3'b111: result <= 4'b0000;
+                    default: result <= 4'b0010;
                 endcase
-            default: result = 4'b0010;
+            default: result <= 4'b0010;
         endcase
     end
 
