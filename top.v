3390f89c8081e7be308e570a88d2ae4b07c43a8d
Hik <luiz.alves@playkids.com>
2023-09-26 21:02:40 -0300

    chore: add reset to top

--- top.v
+++ top.v
@@ -1,24 +1,28 @@
 module top
 (
   input clk,
+  input rst,
   output [5:0] led
 );
 wire [31:0] debug; // TODO: hook this into the led
 
 CPU cpu(
   .clk(slowerClk),
+  .rst(~rst),
   .debug(debug)
 );
 
 localparam WAIT_TIME = 13500000;
 reg slowerClk = 0;
-reg [23:0] clockCounter = 0;
+reg [25:0] clockCounter = 0;
+
+assign led = ~debug[5:0];
 
 always @(posedge clk) begin
   clockCounter <= clockCounter + 1;
   if (clockCounter == WAIT_TIME) begin
     clockCounter <= 0;
-    slowerClk <= ~slowerClk;
+    slowerClk <= ~slowerClk;   
   end
 end
 
