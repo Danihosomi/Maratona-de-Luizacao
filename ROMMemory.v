e13529cbe66e08c640dc71ea3d8294e9b636c16a
Gabriel Ribeiro Paiva <gabrielpaiva2002@gmail.com>
2023-09-29 11:18:55 -0300

    feat: add compiled firmware with .asm file

--- ROMMemory.v
+++ ROMMemory.v
@@ -5,14 +5,14 @@ module ROMMemory (
 
 always @* begin
   case(address[9:2]) 
-    0: data = 32'h03300093;
-    1: data = 32'h03200113;
-    2: data = 32'h00209063;
-    3: data = 32'h00112023;
-    4: data = 32'h001120a3;
-    5: data = 32'h00112123;
-    6: data = 32'h001121a3;
-    7: data = 32'h00112223;
+        0: data = 32'h800004b7;
+        1: data = 32'h70000537;
+        2: data = 32'h00000093;
+        3: data = 32'h00052103;
+        4: data = 32'h002080b3;
+        5: data = 32'h0014a023;
+        6: data = 32'hfe000ae3;
+        default: data = 32'h0;
   endcase
 end
 
--- assembly/main.asm
+++ assembly/main.asm
@@ -1,8 +1,8 @@
-addi x1, x0, 51
-addi x2, x0, 50
-bne x1, x2, 0
-sw x1, 0(x2)
-sw x1, 1(x2)
-sw x1, 2(x2)
-sw x1, 3(x2)
-sw x1, 4(x2)
+lui x9, 0x80000
+lui x10, 0x70000
+addi x1, x0, 0
+BUTTON_LOOP:
+lw x2, 0(x10)
+add x1, x1, x2
+sw x1, 0(x9)
+beq x0, x0, BUTTON_LOOP
