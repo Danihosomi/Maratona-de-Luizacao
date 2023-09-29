84e55d2ae760c4a190b03dc6e80bf87565dc0590
Vinícius Alves Damasceno <vinidamasceno2010@gmail.com>
2023-09-28 21:24:05 -0300

    feat: add m extension instructions in a dumb way

--- alu.v
+++ alu.v
@@ -37,6 +37,15 @@ always @* begin
     6'b001100: resultALU = (operand1 < operand2) ? 0 : 1;  // 1100: BLTU
     6'b001101: resultALU = (operand1 >= operand2) ? 0 : 1; // 1101: BGEU
 
+    6'b001000: resultALU = operand1 * operand2; //mul
+    6'b001001: resultALU = (operand1 * operand2) >> 32; //mulh
+    6'b001010: resultALU = (operand1 * operand2) >> 32; //mulhsu
+    6'b001011: resultALU = (operand1 * operand2) >> 32; //mulhu
+    6'b001100: resultALU = (operand1 / operand2); //div
+    6'b001101: resultALU = (operand1 / operand2); //divu
+    6'b001110: resultALU = (operand1 % operand2); //rem
+    6'b001111: resultALU = (operand1 % operand2); //remu
+
     default: resultALU = operand1 + operand2;
   endcase
 
