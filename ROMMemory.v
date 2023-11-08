ea7bc11ac457e824813a8b50b9fc444c76f57dde
Gabriel Ribeiro Paiva <g223475@dac.unicamp.br>
2023-11-08 16:35:47 -0300

    feat: implement MMU integration with CPU

--- ROMMemory.v
+++ ROMMemory.v
@@ -6,23 +6,13 @@ module ROMMemory (
 always @* begin
   case(address[9:2]) 
         0: data = 32'h00a00093;
-        1: data = 32'h00f00113;
-        2: data = 32'h01400193;
-        3: data = 32'h01900213;
-        4: data = 32'h800004b7;
-        5: data = 32'h0030e093;
-        6: data = 32'h00317113;
-        7: data = 32'h40118233;
-        8: data = 32'h003201b3;
-        9: data = 32'h0014a023;
-        10: data = 32'h0024a023;
-        11: data = 32'h0034a023;
-        12: data = 32'h0044a023;
-        13: data = 32'h40002283;
-        14: data = 32'h40402303;
-        15: data = 32'h40802383;
-        16: data = 32'h40c02403;
-        17: data = 32'h00000013;
+        1: data = 32'h40000113;
+        2: data = 32'h00300193;
+        3: data = 32'h00112023;
+        4: data = 32'h00012203;
+        5: data = 32'hfff18193;
+        6: data = 32'h00a08093;
+        7: data = 32'hfe3018e3;
         default: data = 32'h0;
   endcase
 end
