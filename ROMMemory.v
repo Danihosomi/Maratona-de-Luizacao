9ec4986963029299de2f96690c5b464d760a651b
Hik <luiz.alves@playkids.com>
2023-11-10 01:41:42 -0300

    chore: use compact instructions as test

--- ROMMemory.v
+++ ROMMemory.v
@@ -4,14 +4,16 @@ module ROMMemory (
 );
 
 always @* begin
-  case(address[9:2]) 
-        0: data = 32'h800004b7;
-        1: data = 32'h70000537;
-        2: data = 32'h00000093;
-        3: data = 32'h00052103;
-        4: data = 32'h002080b3;
-        5: data = 32'h0014a023;
-        6: data = 32'hfe000ae3;
+  case(address[9:1]) 
+        0: data = 32'h00000013;
+        1: data = 32'h908a0000;
+        2: data = 32'h0013908a;
+        3: data = 32'h00000013;
+        4: data = 32'h00130000;
+        5: data = 32'h00000013;
+        6: data = 32'h80b30000;
+        7: data = 32'h002080b3;
+        8: data = 32'h0020;
         default: data = 32'h0;
   endcase
 end
--- assembly/main.asm
+++ assembly/main.asm
@@ -1,8 +1,5 @@
-lui x9, 0x80000
-lui x10, 0x70000
-addi x1, x0, 0
-BUTTON_LOOP:
-lw x2, 0(x10)
+nop
+c.add x1, x2
+nop
+nop
 add x1, x1, x2
-sw x1, 0(x9)
-beq x0, x0, BUTTON_LOOP
