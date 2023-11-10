390e84355ccf76dc74fa22ca67fb26f7bb14f113
LarissaEscaliante <larissastefano12@gmail.com>
2023-11-10 15:05:45 -0300

    fixed program

--- ROMMemory.v
+++ ROMMemory.v
@@ -5,14 +5,14 @@ module ROMMemory (
 
 always @* begin
   case(address[9:2]) 
-        0: data = 32'h60000137;
+        0: data = 32'ha0000137;
         1: data = 32'h00000093;
         2: data = 32'h00112023;
         3: data = 32'h000000b3;
         4: data = 32'h00110113;
         5: data = 32'h00112023;
         6: data = 32'h00018233;
-        7: data = 32'h700002b7;
+        7: data = 32'h900002b7;
         8: data = 32'h0002a183;
         9: data = 32'hfe418ae3;
         10: data = 32'hfe0188e3;
--- assembly/main.asm
+++ assembly/main.asm
@@ -1,4 +1,4 @@
-lui x2, 0x60000
+lui x2, 0xA0000
 addi x1, x0, 0b00000000
 sw x1, 0(x2)
 add x1, x0, x0
@@ -6,7 +6,7 @@ addi x2, x2, 1
 sw x1, 0(x2)
 start:
 add x4, x3, x0
-lui x5, 0x70000
+lui x5, 0x90000
 lw x3, 0(x5)
 beq x3, x4, start
 beq x3, x0, start
