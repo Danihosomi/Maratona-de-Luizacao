c5b2150ae1b2d647eee5225b6ce861ae3d36a268
LarissaEscaliante <larissastefano12@gmail.com>
2023-11-10 14:22:15 -0300

    fixed constraints and code

--- ROMMemory.v
+++ ROMMemory.v
@@ -5,11 +5,20 @@ module ROMMemory (
 
 always @* begin
   case(address[9:2]) 
-        0: data = 32'h0af00093;
-        1: data = 32'h60000137;
+        0: data = 32'h60000137;
+        1: data = 32'h00000093;
         2: data = 32'h00112023;
-        3: data = 32'h00110113;
-        4: data = 32'h00112023;
+        3: data = 32'h000000b3;
+        4: data = 32'h00110113;
+        5: data = 32'h00112023;
+        6: data = 32'h00018233;
+        7: data = 32'h700002b7;
+        8: data = 32'h0002a183;
+        9: data = 32'hfe418ae3;
+        10: data = 32'hfe0188e3;
+        11: data = 32'h00108093;
+        12: data = 32'h00112023;
+        13: data = 32'hfe0002e3;
         default: data = 32'h0;
   endcase
 end
--- assembly/main.asm
+++ assembly/main.asm
@@ -1,5 +1,15 @@
-addi x1, x0, 0b10101111
 lui x2, 0x60000
+addi x1, x0, 0b00000000
 sw x1, 0(x2)
+add x1, x0, x0
 addi x2, x2, 1
 sw x1, 0(x2)
+start:
+add x4, x3, x0
+lui x5, 0x70000
+lw x3, 0(x5)
+beq x3, x4, start
+beq x3, x0, start
+addi x1, x1, 1
+sw x1, 0(x2)
+beq x0, x0, start
