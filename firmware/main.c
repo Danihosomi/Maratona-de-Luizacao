cee30dff388efa2b2f667d24c33c774ecaa6d63a
LarissaEscaliante <larissastefano12@gmail.com>
2023-11-24 15:42:14 -0300

    implemented driver

--- firmware/main.c
+++ firmware/main.c
@@ -2,8 +2,12 @@
 int main (void) __attribute__ ((section (".text.entrypoint")));
 
 int* LED_ADDRESS = (int*) (0b1000 << 28);
+int* MATRIX_ADDRESS = (int*) (0b1010 << 28);
 
 void display_led(int);
+void displayMatrix(int** matrix);
+
+#define setBit(number, i) (number |= (1 << i)) 
 
 int main() {
   for (int i = 0; i < 4; i++) {
@@ -15,6 +19,24 @@ int main() {
 // *** DRIVERS ***
 void display_led(int number) {
   *LED_ADDRESS = number;
+}
+
+void displayCell(int i, int j, int** matrix) {
+  int value = 0;
+  setBit(value, i);
+  setBit(value, j + 8);
+  *MATRIX_ADDRESS = value;
+}
+
+void displayMatrix(int** matrix) {
+  for(int i = 0; i < 8; i++) {
+    for(int j = 0; j < 8; j++) {
+      if(matrix[i][j]) {
+        displayCell(i, j, matrix);
+      }
+    }
+  }
+  *MATRIX_ADDRESS = 0;
 }
 
 void _start() {
