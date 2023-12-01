00ddbd18cadf3c950cd02ecdce693f60fcb2f392
Gabriel Ribeiro Paiva <g223475@dac.unicamp.br>
2023-11-30 22:35:46 -0300

    feat: doubles memory size

--- scripts/firmware/compile_firmware_c.sh
+++ scripts/firmware/compile_firmware_c.sh
@@ -6,6 +6,6 @@ export TARGET_FILENAME=$(basename ${TARGET})
 
 echo $TARGET_FILENAME
 
-riscv64-unknown-elf-gcc -g -march=rv32i -std=gnu99 -mabi=ilp32 firmware/${TARGET} -o temp/${TARGET_FILENAME}.riscv -g -march=rv32i -std=gnu99 -mabi=ilp32 -nostartfiles -nostdinc -Ttext=8 -Tdata=700 -ffunction-sections;
+riscv64-unknown-elf-gcc -g -march=rv32i -std=gnu99 -mabi=ilp32 firmware/${TARGET} -o temp/${TARGET_FILENAME}.riscv -g -march=rv32i -std=gnu99 -mabi=ilp32 -nostartfiles -nostdinc -Ttext=8 -Tdata=e00 -ffunction-sections;
 riscv64-unknown-elf-objdump -d temp/${TARGET_FILENAME}.riscv  > temp/${TARGET_FILENAME}.dump;
-elf2hex 4 512 temp/${TARGET_FILENAME}.riscv > ${ROM_PATH};
+elf2hex 4 1024 temp/${TARGET_FILENAME}.riscv > ${ROM_PATH};
